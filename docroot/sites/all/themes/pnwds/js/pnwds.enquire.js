e4611317b6043a7b82168ea892d58f8845346b9a
Erik Baldwin <ThatGuy@ErikBaldwin.com>
2014-07-17 01:40:31 -0700

    style and script updates for short pages

--- docroot/sites/all/themes/pnwds/js/pnwds.enquire.js
+++ docroot/sites/all/themes/pnwds/js/pnwds.enquire.js
@@ -8,12 +8,16 @@
     
     // Variables for DOM elements
     var w = window,
+        wH = w.innerHeight,
         d = document,
         b = d.getElementsByTagName('body'),
+        container = d.getElementById('container'),
         mainNav = d.getElementById('block-system-main-menu'),
         userNav = d.getElementById('block-system-user-menu'),
         menuRegion = d.getElementsByClassName('l-region--menu'),
-        navRegion = d.getElementsByClassName('l-region--navigation');
+        navRegion = d.getElementsByClassName('l-region--navigation'),
+        footerWrapper = d.getElementsByClassName('w-footer'),
+        footerOrigClass = footerWrapper.className;
     
     enquire.register("screen and (max-width:56.25em)", {
       match : function() {
@@ -25,7 +29,24 @@
       setup : function() {},
       deferSetup : true,
       destroy : function() {}
-    });
+    }).register("screen and (min-width:56.25em)", {
+      match : function() {
+        footerWrapper[0].className = footerWrapper[0].className + " positioned";
+        footerHeight = footerWrapper[0].offsetHeight;
+        container.style.paddingBottom = parseInt(footerHeight) + "px";
+        
+        if (wH > container.offsetHeight) {
+          container.style.height = wH + "px";
+        }
+      },
+      unmatch : function() {
+        footerWrapper[0].className = footerOrigClass;
+        container.style.paddingBottom = "0px";
+      },
+      setup : function() {},
+      deferSetup : true,
+      destroy : function() {}
+    });;
     
   });
 
