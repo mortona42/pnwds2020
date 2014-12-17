773f65e2dc10bfe2beddef6fa4daefc204fba675
Jakob Perry <japerry@45640.no-reply.drupal.org>
2014-12-17 11:15:37 -0800

    COD Beta 5 dev, December 2014 Updates

--- docroot/profiles/cod/modules/contrib/ticket/theme/ticket-type.tpl.php
+++ docroot/profiles/cod/modules/contrib/ticket/theme/ticket-type.tpl.php
@@ -12,7 +12,8 @@
  */
 ?>
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <h3><?php print render($content['ticket_type_label']); ?></h3>
   <div class="content"<?php print $content_attributes; ?>>
-    <?php print render($content); ?>
+    <?php print render($content['ticket_type_description']); ?>
   </div>
 </div>
