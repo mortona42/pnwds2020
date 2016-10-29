66bbb362569e288950e205f26f31d6d9df1807c2
Joël Pittet <pittet@cs.ubc.ca>
2016-10-29 16:27:21 -0700

    make less dom searches with reference variables and trigger click on active trail toggles

--- docroot/sites/all/themes/cascadia/js/cascadia.behaviors.js
+++ docroot/sites/all/themes/cascadia/js/cascadia.behaviors.js
@@ -46,68 +46,68 @@
       // processed previously. By using .once('foo') all processed elements will
       // get tagged with a 'foo-processed' class, causing all future invocations
       // of this behavior to ignore them.
-      $('.some-selector', context).once('foo', function () {
-        // Now, we are invoking the previously declared theme function using two
-        // settings as arguments.
-        var $anchor = Drupal.theme('cascadiaExampleButton', settings.myExampleLinkPath, settings.myExampleLinkTitle);
 
-        // The anchor is then appended to the current element.
-        $anchor.appendTo(this);
-      });
-
-
-      // Scroll to top
+      // Scroll to top.
       $('a.js-page-top').on('click', function(event) {
         event.preventDefault();
         $('html,body').animate({ scrollTop: 0 }, 400);
       });
 
-      // Scroll to hash url
+      // Scroll to hash url.
       $('a[href^="#"]').on('click', function(event) {
         var target = $(this.getAttribute('href'));
         if(target.length) {
           event.preventDefault();
           var targetOffset = target.offset().top;
           $('html, body').stop().animate({
-              scrollTop: targetOffset - 80
+            scrollTop: targetOffset - 80
           }, 400);
         }
       });
 
       // Mobile menu toggle
       $('.js-menu-toggle').on('click', function(event) {
-        if ($(this).is('.js-menu-toggle-active')) {
-          $(this).removeClass('js-menu-toggle-active');
-          $(this).removeClass('menu__button-toggle--active');
-          $('.l-region--navigation').slideUp('fast');
+        var $toggle = $(this);
+        var $navRegion = $('.l-region--navigation');
+        if ($toggle.is('.js-menu-toggle-active')) {
+          $toggle.removeClass('js-menu-toggle-active');
+          $toggle.removeClass('menu__button-toggle--active');
+          $navRegion.slideUp('fast');
         } else {
-          $(this).addClass('js-menu-toggle-active');
-          $(this).addClass('menu__button-toggle--active');
-          $('.l-region--navigation').slideDown('fast');
+          $toggle.addClass('js-menu-toggle-active');
+          $toggle.addClass('menu__button-toggle--active');
+          $navRegion.slideDown('fast');
         }
       });
 
-      // Sub-menu click toggle
+      // Sub-menu click toggle.
+      var $menuItemParents = $('.menu-item--parent');
       $('.menu-item--parent .js-menu-item-toggle').on('click', function () {
-        if ($(this).is('.js-menu-item-toggle-active')) {
-          // COLLAPSE SUB-MENU
-          $(this).removeClass('js-menu-item-toggle-active');
-          $(this).closest('.menu-item--parent').removeClass('menu-item--active');
-          $(this).siblings('ul.menu').slideUp('fast');
+        var $toggle = $(this);
+        var $parent = $toggle.closest('.menu-item--parent');
+        var $siblings = $toggle.siblings('ul.menu');
+        if ($toggle.is('.js-menu-item-toggle-active')) {
+          // COLLAPSE SUB-MENU.
+          $toggle.removeClass('js-menu-item-toggle-active');
+          $parent.removeClass('menu-item--active');
+          $siblings.slideUp('fast');
         } else {
-          // EXPAND SUB-MENU
-          // Close open sub-menu
-          $('.menu-item--parent').each(function () {
-            $(this).find('.js-menu-item-toggle').removeClass('js-menu-item-toggle-active');
-            $(this).removeClass('menu-item--active');
-            $(this).find('ul.menu').slideUp('fast');
-          });
-          // Open new sub-menu
-          $(this).addClass('js-menu-item-toggle-active');
-          $(this).closest('.menu-item--parent').addClass('menu-item--active');
-          $(this).siblings('ul.menu').slideDown('fast');
+          // EXPAND SUB-MENU.
+          // Close open sub-menu.
+          $menuItemParents.find('.js-menu-item-toggle').removeClass('js-menu-item-toggle-active');
+          $menuItemParents.removeClass('menu-item--active');
+          $menuItemParents.find('ul.menu').slideUp('fast');
+          // Open new sub-menu.
+          $toggle.addClass('js-menu-item-toggle-active');
+          $parent.addClass('menu-item--active');
+          $siblings.slideDown('fast');
         }
       });
+
+      // Toggle active trail on load.
+      var $activeTrailToggle = $menuItemParents.children('.active-trail').siblings('.js-menu-item-toggle');
+      $activeTrailToggle.trigger('click');
+
       // Sub-menu hover toggle for desktop
       // $('.menu-item--parent').on('mouseenter', function () {
       //   if ($(window).width() >= 770) {
